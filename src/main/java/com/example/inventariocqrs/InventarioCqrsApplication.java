0f0421a202cba2fc8b0fd1b728e9436a42ccb8d0
Erick <erimatoja@gmail.com>
2026-04-09 11:21:56 -0500

    Commit 1: Estructura Maven base y configuración Spring Boot

---
+++ src/main/java/com/example/inventariocqrs/InventarioCqrsApplication.java
@@ -0,0 +1,12 @@
+package com.example.inventariocqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventarioCqrsApplication.class, args);
+    }
+}
